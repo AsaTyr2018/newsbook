a88739dc456f701f81c8a191889c5c34409db404
AsaTyr <43162495+AsaTyr2018@users.noreply.github.com>
2025-09-13 00:40:21 +0200

    Show only pending comments in moderation

--- pages/api/comments.ts
+++ pages/api/comments.ts
@@ -6,9 +6,18 @@ import { authOptions } from './auth/[...nextauth]';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const session = await getServerSession(req, res, authOptions);
   if (req.method === 'GET') {
-    const { postId } = req.query;
+    const { postId, status } = req.query;
     const where: any = postId ? { postId: Number(postId) } : {};
-    if (!session || !['MODERATOR', 'ADMIN'].includes((session.user as any).role)) {
+    if (
+      status &&
+      session &&
+      ['MODERATOR', 'ADMIN'].includes((session.user as any).role)
+    ) {
+      where.status = String(status);
+    } else if (
+      !session ||
+      !['MODERATOR', 'ADMIN'].includes((session.user as any).role)
+    ) {
       where.status = 'APPROVED';
     }
     const comments = await prisma.comment.findMany({
